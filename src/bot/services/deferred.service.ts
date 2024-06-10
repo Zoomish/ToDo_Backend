0d520ac230f6b7dc592e0958c4bd9cbf30a99262
Zoom <egor147536987@gmail.com>
2024-06-10 14:23:00 +0300

    "Added @michpl/telegram-calendar dependency and integrated it into DeferredService to send calendar markup with messages"

--- src/bot/services/deferred.service.ts
+++ src/bot/services/deferred.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common'
 import schedule = require('node-schedule')
+import { Calendar } from '@michpl/telegram-calendar'
 
 @Injectable()
 export class DeferredService {
     async sendTime(time, msg, text, bot) {
+        const calendar = new Calendar()
         const date = +new Date() + time * 60 * 1000
         new schedule.scheduleJob(date, function () {
-            bot.sendMessage(msg.chat.id, text)
+            bot.sendMessage(msg.chat.id, text, {
+                parse_mode: 'html',
+                reply_markup: JSON.stringify({
+                    inline_keyboard: calendar.getPage(new Date()),
+                }),
+            })
         })
     }
 }
