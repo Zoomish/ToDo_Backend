ab87ca1e1fe5e2708292c1467a73f540507aa660
Zoom <egor147536987@gmail.com>
2024-06-10 19:11:02 +0300

    "Moved calendar initialization to class property and updated scheduleJob callback to arrow function"

--- src/bot/services/deferred.service.ts
+++ src/bot/services/deferred.service.ts
@@ -4,16 +4,16 @@ import { Calendar } from '@michpl/telegram-calendar'
 
 @Injectable()
 export class DeferredService {
+    calendar = new Calendar({
+        minDate: new Date(),
+    })
     async sendTime(time, msg, text, bot) {
-        const calendar = new Calendar({
-            minDate: new Date(),
-        })
         const date = +new Date() + time * 60 * 1000
-        new schedule.scheduleJob(date, function () {
+        new schedule.scheduleJob(date, () => {
             bot.sendMessage(msg.chat.id, text, {
                 parse_mode: 'html',
                 reply_markup: JSON.stringify({
-                    inline_keyboard: calendar.getPage(new Date()),
+                    inline_keyboard: this.calendar.getPage(new Date()),
                     callback_data: 'date',
                 }),
             })
