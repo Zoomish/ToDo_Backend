d67a234123531cb5715b01e1d3f406d394dcf709
Zoom <egor147536987@gmail.com>
2024-06-10 18:58:21 +0300

    "Updated BotService to send message with 0.01 minute delay, added DeferredService to CallbackService constructor, and modified CallbackService to handle calendar actions. Also, updated DeferredService to set minDate for Calendar and add callback_data to reply_markup

--- src/bot/services/callback.service.ts
+++ src/bot/services/callback.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { ProjectService } from './project.service'
+import { DeferredService } from './deferred.service'
 
 @Injectable()
 export class CallbackService {
-    constructor(private readonly projectService: ProjectService) {}
+    constructor(
+        private readonly projectService: ProjectService,
+        private readonly deferredService: DeferredService
+    ) {}
     async callback(bot, callbackQuery) {
         const action = callbackQuery.data
         const msg = callbackQuery.message
@@ -12,6 +16,21 @@ export class CallbackService {
                 return await this.projectService.getProjects(bot, msg)
             default:
                 break
+        }
+        const calendar = JSON.parse(action)
+        if (typeof calendar === 'object' && calendar.type === 'calendar') {
+            console.log(calendar.action)
+
+            switch (calendar.action) {
+                case null:
+                    return console.log(1)
+                case 'next-month':
+                    return console.log(2)
+                case 'select-year':
+                    return console.log(3)
+                default:
+                    break
+            }
         }
     }
 }
--- src/bot/services/deferred.service.ts
+++ src/bot/services/deferred.service.ts
@@ -5,13 +5,16 @@ import { Calendar } from '@michpl/telegram-calendar'
 @Injectable()
 export class DeferredService {
     async sendTime(time, msg, text, bot) {
-        const calendar = new Calendar()
+        const calendar = new Calendar({
+            minDate: new Date(),
+        })
         const date = +new Date() + time * 60 * 1000
         new schedule.scheduleJob(date, function () {
             bot.sendMessage(msg.chat.id, text, {
                 parse_mode: 'html',
                 reply_markup: JSON.stringify({
                     inline_keyboard: calendar.getPage(new Date()),
+                    callback_data: 'date',
                 }),
             })
         })
