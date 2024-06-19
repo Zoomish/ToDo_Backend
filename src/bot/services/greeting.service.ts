26fad83be6127470531a512b7a269fdcd71a7bad
Zoom <egor147536987@gmail.com>
2024-06-20 02:28:44 +0300

    "Removed console log, updated callback data, and added new message with inline keyboard for registration and authorization"

--- src/bot/services/greeting.service.ts
+++ src/bot/services/greeting.service.ts
@@ -5,7 +5,6 @@ export class GreetingService {
     constructor() {}
     async greeting(bot, chatId, msg) {
         console.log(msg)
-
         await bot.sendMessage(
             chatId,
             `Здравствуйте, ${msg?.chat?.first_name}! Это мой бот(@Zoomish). Он был написан для вашего удобства в планировании своих дел\n\n/help - для просмотра команд`,
@@ -20,9 +19,35 @@ export class GreetingService {
                             msg.from.id === 1432248791
                                 ? {
                                       text: 'Получить всех пользователей',
-                                      callback_data: 'getAll',
+                                      callback_data: 'get_all',
                                   }
                                 : '',
+                        ],
+                    ],
+                },
+            }
+        )
+        await bot.sendMessage(
+            chatId,
+            `Для начала, зарегистрируйтесь или войдите в уже существующий аккаунт`,
+            {
+                reply_markup: {
+                    keyboard: [
+                        [
+                            {
+                                text: 'Войти в аккаунт',
+                                web_app: {
+                                    url:
+                                        process.env.URL + '/admin/autorization',
+                                },
+                            },
+                            {
+                                text: 'Зарегестрироваться',
+                                web_app: {
+                                    url:
+                                        process.env.URL + '/admin/registration',
+                                },
+                            },
                         ],
                     ],
                 },
