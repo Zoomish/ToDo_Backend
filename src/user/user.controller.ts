9c65c03a453b400ef9f64c2380a1d736b33498e8
Zoom <egor147536987@gmail.com>
2024-06-20 01:17:12 +0300

    "Remove CreateUserDto import and create method from UserController"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,10 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common'
-import { CreateUserDto } from './dto/create-user.dto'
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UpdateUserDto } from './dto/update-user.dto copy'
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
-
-    @Post()
-    create(@Body() createUserDto: CreateUserDto) {
-        return this.userService.create(createUserDto)
-    }
 
     @Get()
     findAll() {
