d7c1869c3b66e9215eeff19ba189873f106a8913
Zoom <egor147536987@gmail.com>
2024-06-20 15:28:47 +0300

    "Added null check for user.tg_id before updating it in WebUserService"

--- src/bot/services/web/webuser.service.ts
+++ src/bot/services/web/webuser.service.ts
@@ -6,8 +6,10 @@ export class WebUserService {
     constructor(private readonly userService: UserService) {}
     async addTdIdUser(bot, chatId, data, userTgId) {
         const user = await this.userService.findByLogin(data.email)
-        user.tg_id = userTgId
-        user.save()
+        if (user.tg_id !== null) {
+            user.tg_id = userTgId
+            user.save()
+        }
         await bot.sendMessage(chatId, `Вы успешно зарегистрировались!`)
         await bot.sendMessage(chatId, JSON.stringify(user))
     }
