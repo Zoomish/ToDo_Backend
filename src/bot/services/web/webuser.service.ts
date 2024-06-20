1e1930da393394e4b9f5f037f1aab40920740c86
Zoom <egor147536987@gmail.com>
2024-06-20 16:53:02 +0300

    "Added UserService dependency to WebUserService, updated addTdIdUser method to use UserService, and changed signIn and signUp methods to use email from data instead of user object."

--- src/bot/services/web/webuser.service.ts
+++ src/bot/services/web/webuser.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common'
+import { UserService } from '../../../user/user.service'
 import { AuthService } from 'src/auth/auth.service'
 
 @Injectable()
 export class WebUserService {
-    constructor(private readonly authService: AuthService) {}
-    private async addTdIdUser(bot, chatId, user, userTgId) {
+    constructor(
+        private readonly userService: UserService,
+        private readonly authService: AuthService
+    ) {}
+    private async addTdIdUser(bot, chatId, email, userTgId) {
+        const user = await this.userService.findByLogin(email)
         if (user.tg_id === null) {
             user.tg_id = userTgId
             user.save()
@@ -15,21 +20,21 @@ export class WebUserService {
         }
     }
     async signIn(bot, chatId, data, userTgId) {
-        const user = await this.authService.login(data)
+        await this.authService.login(data)
         await bot.sendMessage(chatId, `Вы успешно вошли в аккаунт!`, {
             reply_markup: {
                 hide_keyboard: true,
             },
         })
-        return await this.addTdIdUser(bot, chatId, user, userTgId)
+        return await this.addTdIdUser(bot, chatId, data.email, userTgId)
     }
     async signUp(bot, chatId, data, userTgId) {
-        const user = await this.authService.register(data)
+        await this.authService.register(data)
         await bot.sendMessage(chatId, `Вы успешно создали аккаунт!`, {
             reply_markup: {
                 hide_keyboard: true,
             },
         })
-        return await this.addTdIdUser(bot, chatId, user, userTgId)
+        return await this.addTdIdUser(bot, chatId, data.email, userTgId)
     }
 }
