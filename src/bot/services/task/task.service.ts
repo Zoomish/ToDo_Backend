40ab7826214e0522472024a3513efd20298ff2b7
Zoom <egor147536987@gmail.com>
2024-06-21 16:34:51 +0300

    "Updated task message formatting in TasksService to display deadline with weekday and to show 'Нет' if deadline is not set."

--- src/bot/services/task/task.service.ts
+++ src/bot/services/task/task.service.ts
@@ -12,7 +12,16 @@ export class TasksService {
         return await tasks.map((task: Task) => {
             return bot.sendMessage(
                 msg.chat.id,
-                `Заголовок: ${task.title}\nОписание: ${task.description}\nДедлайн: ${new Date(task.time).toDateString()}\nСтатус: ${task.progress}
+                `Заголовок: ${task.title}\nОписание: ${task.description}\nДедлайн: ${
+                    task.time
+                        ? new Date(task.time).toLocaleDateString(undefined, {
+                              weekday: 'long',
+                              year: 'numeric',
+                              month: 'long',
+                              day: 'numeric',
+                          })
+                        : 'Нет'
+                }\nСтатус: ${task.progress}
                 `,
                 {
                     parse_mode: 'HTML',
