53c769b98432a598c58c0e0986260fe8cdbdee84
Zoom <egor147536987@gmail.com>
2024-06-26 14:48:16 +0300

    "Added a function to fetch a URL at a 14 minute and 30 second interval when the app starts"

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { AppModule } from './app.module'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors()
-    await app.listen(process.env.PORT)
+    function aaa() {
+        setInterval(
+            async () => {
+                await fetch(
+                    'https://todo-backend-pwzq.onrender.com/task/get/AAA'
+                )
+            },
+            1000 * 60 * 14 + 1000 * 30
+        )
+    }
+    await app.listen(process.env.PORT, () => aaa())
 }
 bootstrap()
