2703ee958325cc6cdc412bcfa35bdf465c407a5c
Zoom <egor147536987@gmail.com>
2024-06-27 19:02:44 +0300

    "Remove await from notificationService.sendNotification call"

--- src/bot/services/task/task.service.ts
+++ src/bot/services/task/task.service.ts
@@ -13,7 +13,7 @@ export class TasksService {
         const msgWait = await bot.sendMessage(msg.chat.id, `Получаю данные...`)
         await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
         const tasks = await this.taskService.findByUserTgId(msg?.chat?.id)
-        await this.notificationService.sendNotification(bot, msg.chat.id, tasks)
+        this.notificationService.sendNotification(bot, msg.chat.id, tasks)
         return await tasks.map((task: Task) => {
             return bot.sendMessage(
                 msg.chat.id,
