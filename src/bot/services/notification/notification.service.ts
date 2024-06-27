fb9a67428879d83a30b2d8bbf3818bfaadb80bfe
Zoom <egor147536987@gmail.com>
2024-06-27 17:08:20 +0300

    "Refactor NotificationService: removed initial 'Получаю данные...' message, added scheduling of notifications using node-schedule, and updated message formatting"

--- src/bot/services/notification/notification.service.ts
+++ src/bot/services/notification/notification.service.ts
@@ -1,52 +1,63 @@
 import { Injectable } from '@nestjs/common'
 import { Task } from 'src/task/model/task.model'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const schedule = require('node-schedule')
 
 @Injectable()
 export class NotificationService {
     async sendNotification(bot, chatId, tasks) {
-        const msgWait = await bot.sendMessage(chatId, `Получаю данные...`)
-        await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
-        return await tasks.map((task: Task) => {
-            return bot.sendMessage(
-                chatId,
-                `<b>Заголовок:</b> ${task.title}\n<b>Описание:</b> ${task.description ? task.description : 'Нет'}\n<b>Дедлайн:</b> ${
-                    task.time
-                        ? new Date(task.time).toLocaleString(undefined, {
-                              weekday: 'long',
-                              year: 'numeric',
-                              month: 'long',
-                              day: 'numeric',
-                              hour: 'numeric',
-                              minute: 'numeric',
-                          })
-                        : 'Нет'
-                }\n<b>Статус:</b> ${task.progress}
-                `,
-                {
-                    parse_mode: 'HTML',
-                    protect_content: true,
-                    reply_markup: JSON.stringify({
-                        inline_keyboard: [
-                            [
-                                {
-                                    text: 'Изменить задачу',
-                                    web_app: {
-                                        url: process.env.URL,
-                                    },
-                                },
-                            ],
-                            [
-                                {
-                                    text: 'Удалить задачу',
-                                    web_app: {
-                                        url: process.env.URL,
-                                    },
-                                },
-                            ],
-                        ],
-                    }),
-                }
-            )
-        })
+        await bot.sendMessage(
+            chatId,
+            `Уведомления установлены для всех имеющихся задач`
+        )
+        return await tasks
+            .filter((task: Task) => task.notification !== null)
+            .map((task: Task) => {
+                schedule.scheduleJob(task.notification, async () => {
+                    return bot.sendMessage(
+                        chatId,
+                        `<b>Заголовок:</b> ${task.title}\n<b>Описание:</b> ${task.description ? task.description : 'Нет'}\n<b>Дедлайн:</b> ${
+                            task.time
+                                ? new Date(task.time).toLocaleString(
+                                      undefined,
+                                      {
+                                          weekday: 'long',
+                                          year: 'numeric',
+                                          month: 'long',
+                                          day: 'numeric',
+                                          hour: 'numeric',
+                                          minute: 'numeric',
+                                      }
+                                  )
+                                : 'Нет'
+                        }\n<b>Статус:</b> ${task.progress}
+                    `,
+                        {
+                            parse_mode: 'HTML',
+                            protect_content: true,
+                            reply_markup: JSON.stringify({
+                                inline_keyboard: [
+                                    [
+                                        {
+                                            text: 'Изменить задачу',
+                                            web_app: {
+                                                url: process.env.URL,
+                                            },
+                                        },
+                                    ],
+                                    [
+                                        {
+                                            text: 'Удалить задачу',
+                                            web_app: {
+                                                url: process.env.URL,
+                                            },
+                                        },
+                                    ],
+                                ],
+                            }),
+                        }
+                    )
+                })
+            })
     }
 }
